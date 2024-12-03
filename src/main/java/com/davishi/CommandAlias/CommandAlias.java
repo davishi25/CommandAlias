61e4a5cd6f5bff203a6e623e60e5d391f0e24200
davishi25 <davishi2008@gmail.com>
2024-12-03 17:45:49 -0600

    made command
    time to figure out how to write and read a text file

--- src/main/java/com/davishi/CommandAlias/CommandAlias.java
+++ src/main/java/com/davishi/CommandAlias/CommandAlias.java
@@ -1,9 +1,11 @@
 package com.davishi.CommandAlias;
 
+import net.minecraftforge.client.ClientCommandHandler;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
 @Mod(modid = CommandAlias.MODID, version = CommandAlias.VERSION, name = CommandAlias.MODNAME, clientSideOnly = true)
 public class CommandAlias
@@ -12,6 +14,12 @@ public class CommandAlias
     public static final String MODNAME = "Copy-Chat";
     public static final String MODID = "commandalias";
     public static final String VERSION = "0.0";
+
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        ClientCommandHandler.instance.registerCommand(new TextMenu());
+    }
 
     @EventHandler
     public void init(FMLInitializationEvent event) {
---
+++ src/main/java/com/davishi/CommandAlias/TextMenu.java
@@ -0,0 +1,45 @@
+package com.davishi.CommandAlias;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.CommandException;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.StringUtils;
+
+public class TextMenu extends CommandBase {
+    @Override
+    public String getCommandName(){return "ca";}
+
+    @Override
+    public String getCommandUsage(ICommandSender sender){return "";}
+
+    @Override
+    public void processCommand(ICommandSender sender, String[] args) throws CommandException {
+
+        if(args.length == 1 && args[0].equals("list")){
+            addMessage(EnumChatFormatting.DARK_AQUA + "---------------");
+            addMessage(EnumChatFormatting.AQUA + "Command Aliases");
+            addMessage(EnumChatFormatting.DARK_AQUA + "---------------");
+            //print out the array
+        } else if (args.length == 3 && args[0].equals("register")){
+            //register the command into text file
+            if(args[1].charAt(0) != '/' && args[1].length() > 1){
+                addMessage(EnumChatFormatting.RED + "Error: alias must start with /");
+            } else {
+                addMessage(EnumChatFormatting.GREEN + "Successfully registered " + args[1]);
+            }
+        } else{
+            addMessage(EnumChatFormatting.RED + "Wrong Usage");
+            addMessage(EnumChatFormatting.RED + "/ca register <alias> <command>");
+            addMessage(EnumChatFormatting.RED + "/ca list");
+        }
+    }
+    @Override
+    public boolean canCommandSenderUseCommand(ICommandSender sender) { return true; }
+
+    private void addMessage(String s){Minecraft.getMinecraft().thePlayer.addChatMessage(new ChatComponentText(s));}
+}
